fc40bca0346ad73d9e9957db22a80f01ba3134ca
koldomasov.nd <koldomasov.nd@dozen.mephi.ru>
2024-11-26 23:00:51 +0300

    Implement SpawnAction

---
+++ action_types/spawn_action/spawn_action.cpp
@@ -0,0 +1,26 @@
+#include "spawn_action.hpp"
+#include <memory>
+#include <utility>
+#include "mob.hpp"
+#include "mob_plugin.hpp"
+#include "stage.hpp"
+
+namespace rln {
+
+SpawnAction::SpawnAction(Game* game,
+                         Point pos,
+                         std::shared_ptr<Entity> entity,
+                         const json& data)
+    : EntityAction(game, pos, entity), data_(data) {}
+
+ActionResult SpawnAction::perform() {
+    std::string id = data_["id"];
+    auto& plugin = game()->plugin(id);
+    auto spawned_mob = plugin.create_mob(data_);
+    spawned_mob->position(pos());
+    game()->stage()->add_entity(spawned_mob);
+    add_event(Event(Event::EventType::SPAWN, spawned_mob, pos()));
+    return ActionResult::succeed();
+}
+
+}  // namespace rln
--- action_types/spawn_action/spawn_action.hpp
+++ action_types/spawn_action/spawn_action.hpp
@@ -1,4 +1,29 @@
 #pragma once
 
-// TODO
-class SpawnAction;
+#include "entity_action.hpp"
+#include <memory>
+
+
+
+// remove when json placeholder is removed
+#include <string>
+
+namespace rln {
+
+// TODO: include json library
+class json {
+public:
+    std::string operator[](const std::string&) const;
+};
+
+class SpawnAction : public EntityAction {
+public:
+    SpawnAction(Game* game, Point pos, std::shared_ptr<Entity> entity, const json& data);
+
+    ActionResult perform() override;
+
+private:
+    json data_;
+};
+
+}
