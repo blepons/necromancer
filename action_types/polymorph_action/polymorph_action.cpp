955a052251cc4dc0f690300bb0050438bfb7c34e
koldomasov.nd <koldomasov.nd@dozen.mephi.ru>
2024-11-30 23:04:27 +0300

    Implement PolymorphAction

---
+++ action_types/polymorph_action/polymorph_action.cpp
@@ -0,0 +1,32 @@
+#include "polymorph_action.hpp"
+#include <algorithm>
+#include <memory>
+#include "entity.hpp"
+#include "mob.hpp"
+#include "mob_plugin.hpp"
+#include "stage.hpp"
+
+namespace rln {
+
+PolymorphAction::PolymorphAction(Game* game,
+                                 Point pos,
+                                 std::shared_ptr<Entity> entity,
+                                 const json& data)
+    : EntityAction(game, pos, entity), data_(data) {}
+
+ActionResult PolymorphAction::perform() {
+    float hp_fraction =
+        static_cast<float>(entity()->health()) / entity()->max_health();
+    std::string id = data_["id"];
+    auto& plugin = game()->plugin(id);
+    std::shared_ptr<Mob> spawned_mob = plugin.create_mob(data_);
+    spawned_mob->health(
+        std::max(static_cast<int>(hp_fraction * spawned_mob->max_health()), 1));
+    spawned_mob->energy() = entity()->energy();
+    game()->stage()->replace_entity(spawned_mob, entity()->position());
+    add_event(Event(Event::EventType::POLYMORPH, spawned_mob,
+                    spawned_mob->position()));
+    return ActionResult::succeed();
+}
+
+}  // namespace rln
--- action_types/polymorph_action/polymorph_action.hpp
+++ action_types/polymorph_action/polymorph_action.hpp
@@ -1,4 +1,26 @@
 #pragma once
 
-// TODO
-class PolymorphAction;
+#include <memory>
+#include <nlohmann/json.hpp>
+#include "entity_action.hpp"
+
+using json = nlohmann::json;
+
+namespace rln {
+
+class PolymorphAction : public EntityAction {
+public:
+    PolymorphAction(Game* game,
+                    Point pos,
+                    std::shared_ptr<Entity> entity,
+                    // std::string_view kind);
+                    const json& data);
+
+    ActionResult perform() override;
+
+private:
+    std::string kind_;
+    json data_;
+};
+
+}  // namespace rln
